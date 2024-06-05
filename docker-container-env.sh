c3d7604512a07c3d903196d5fba8a2d4f6918f9e
Iko Afianando <ikoafianando123@gmail.com>
2024-06-05 21:49:57 +0700

    update: docker-container .env

--- docker-container-env.sh
+++ docker-container-env.sh
@@ -3,3 +3,4 @@
 #docker container create --name contohmongo --publish 27017:27017 --env MONGO_INITDB_ROOT_USERNAME=iko --env MONGO_INITDB_ROOT_PASSWORD=iko mongo:latest
 
 docker container ls -a;
+docker container ls;
